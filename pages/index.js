6ebe6557b7635a6ea9cdd8edd4f7b100eafd4d1c
Handerson Junior <misterdel04@hotmail.com>
2022-05-19 21:23:10 +0100

    commtit na branch teste

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,30 @@
+import styles from '../styles/Home.module.css'
 
-import {useState} from 'react'
-function Home(){
-    return (
-        <div>
-            <h2>Home</h2>
-            <Contador />
-        </div>
-    )
+export default function Home(props) {
+  const dynamicDate = new Date();
+  const dynamicDateString = dynamicDate.toGMTString();
+  return (
+    <div >
+       <h1 className={styles.title}>Hello Word Next.js</h1>
+       <div>  {dynamicDateString} (dinâmico) </div>
+       <div>  {props.staticDateString} (estatico) </div>
+
+    </div>
+  )
 }
 
-
-function Contador(){
-const [contador, setContador] = useState(1);
-function adicionarContador(){
-    setContador(contador +1);
-
+/**
+ * props guarda os dados para caso nao queres que quando o site cai, percas os dados na tela ou seja, elas sejam apresentados na mesma
+ * isso pork ela é carregado e guardado logo no deploy da aplicação
+ * @returns 
+ */
+export function getStaticProps(){
+  
+  const staticDate = new Date();
+  const staticDateString = staticDate.toGMTString();
+  return {
+    props: {
+      staticDateString
+    }
+  }
 }
-
-    return(
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-}
-
-
-
-export default Home
---
+++ pages/produts/pants/bluepaints.js
@@ -0,0 +1,3 @@
+export default function BluePaints(){
+    return <h1>Calça azul - 19.99€</h1>
+}
